eaab9988e1f05f544603f5dc30ddf207a75dd690
Endris Kraig <EndrisKraig@gmail.com>
2022-03-29 19:37:18 +0300

    navigation bar

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  useParams,
+  useLocation,
 } from "react-router-dom";
 
 import CatAdder from "./component/CatAdder";
 import CatDetails from "./component/CatDetails";
 import MainScreen from "./component/MainScreen";
 import Login from "./component/Login"
+import NavigationBar from "./component/NavigationBar";
 
 export default function App() {
   const {token, setToken} = useToken();
-
   if(!token){
     return <Login setToken={setToken} />
   }
 
   return (
     <div>
+      <NavigationBar/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainScreen />} />
---
+++ src/component/NavigationBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import styles from "./NavigatorBar.module.css"
+
+const links = ["Home", "Cats", "Login"];
+
+export default function NavigationBar(props) {
+    console.log(props.path);
+    const [elems, setElems] = useState({ "active": links[0] });
+    const activeElem = elems.active;
+
+    const renderLinks = links.map((link) => {
+        if (link === activeElem) {
+            return <a key={link} className={styles.active}>{link}</a>
+        } else {
+            return <a key={link} className={styles.a} onClick={() => setElems({"active":link})}>{link}</a>
+        }
+    })
+    return (
+        <div className={styles.topnav}>
+            {renderLinks}
+        </div>
+    );
+}

5d9c1dc84edc4bd55dc28dfefb1900c3228dddb4
Endris Kraig <EndrisKraig@gmail.com>
2022-04-23 14:32:06 +0300

    landing like main page

--- src/component/MainScreen.js
+++ src/component/MainScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
-import AnimalSpecials from "./animal/AnimalSpecials";
 import styles from "./MainScreen.module.css"
 
 function MainScreen() {
+  //TODO img size
   return (
-    <div className={styles.text}>
-      Welcome to another pet store!<p/>
-      Please click on menu bar above to proceed further...<p/>
-      <AnimalSpecials/>
+    <div>
+      <div>
+        <img src="animal.jpg" alt="Cat" className={styles.img} />
+        <div className={styles.text_center}>Find your perfect pet in another pet store!</div>
+      </div>
+      <div className={styles.container}>
+        <img src="delivery.jpg" alt="Delivery" className={styles.img} />
+        <div className={styles.text_up_left}>We can deliver your new pet<br/>anywhere and anytime!</div>
+      </div>
     </div>
+
   );
 }
 
